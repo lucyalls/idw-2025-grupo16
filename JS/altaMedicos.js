3a2eceeca23d1b368ab98a1e75a66ad40b8aae8a
lucyalls <lucyallassia12@gmail.com>
2025-10-22 03:19:24 -0300

    Local Storage + login ejemplo + formulario
    Se añadió un archivo localStorage.js para las funciones relacionadas al almacenamiento.
    Se añadió el ejemplo de login dado en clase.
    Se añadieron los formularios para registrar a un médico.

--- JS/altaMedicos.js
+++ JS/altaMedicos.js
@@ -1,7 +1,12 @@
+import { obtenerMedicos, guardarMedicos } from './localStorage.js';
+
 const formAltaMedico = document.getElementById('altaMedicosForm');
 const apellidoInput = document.getElementById('apellido');
 const nombreInput = document.getElementById('nombre');
 const especialidadInput = document.getElementById('especialidad');
+const telefonoInput = document.getElementById('telefono');
+const diasAtencionInput = document.getElementById('dias-atencion');
+const obraSocialInput = document.getElementById('obra-social')
 
 function altaMedicos(event) {
 
@@ -10,15 +15,23 @@ function altaMedicos(event) {
         let apellido = apellidoInput.value.trim();
         let nombre = nombreInput.value.trim();
         let especialidad = especialidadInput.value.trim();
+        let telefono = telefonoInput.value.trim();
+        let diasAtencion = diasAtencionInput.value.trim();
+        let obraSocial = obraSocialInput.value.trim();
 
-        if (!nombre || !apellido || !especialidad) {
+
+        if (!nombre || !apellido || !especialidad || !telefono || !diasAtencion || !obraSocial) {
             alert('Completar los campos obligatorios');
             return;
         }
-        alert( `El médico ${apellido} ${nombre} ha sido dado de alta con la especialidad de ${especialidad}` );
+        alert(
+            `El médico ${apellido} ${nombre} ha sido dado de alta con la especialidad de ${especialidad}\n` +
+            `Teléfono: ${telefono}\n` +
+            `Obras sociales aceptadas: ${obraSocial}\n` +
+            `Días de atención: ${diasAtencion}`
+        );
 
         formAltaMedico.reset();
 }
-
 
 formAltaMedico.addEventListener('submit', altaMedicos);
