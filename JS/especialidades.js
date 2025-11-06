7426aaba5a0e4e0e1ef1aec090ee987298d28aaf
lucyalls <lucyallassia12@gmail.com>
2025-11-06 16:13:40 -0300

    Turnos y Reservas

---
+++ JS/especialidades.js
@@ -0,0 +1,9 @@
+export const especialidades = [
+    { id: 1, nombre: "Cardiología" },
+    { id: 2, nombre: "Neurología" },
+    { id: 3, nombre: "Pediatría" },
+    { id: 4, nombre: "Oncología" },
+    { id: 5, nombre: "Urología" },
+    { id: 6, nombre: "Ginecología" },
+    { id: 7, nombre: "Oftalmología" }
+];
---
+++ JS/turnos.js
@@ -0,0 +1,8 @@
+export const turnos = [
+    { id: 1, idMedico: 1, fechaHora: 'Lunes 10/11 - 09:00 hs', disponible: true },
+    { id: 2, idMedico: 1, fechaHora: 'Lunes 10/11 - 10:00 hs', disponible: true },
+    { id: 3, idMedico: 2, fechaHora: 'Martes 11/11 - 15:00 hs', disponible: true },
+    { id: 4, idMedico: 2, fechaHora: 'Martes 11/11 - 16:00 hs', disponible: true },
+    { id: 5, idMedico: 1, fechaHora: 'Miércoles 12/11 - 11:00 hs', disponible: true },
+    { id: 6, idMedico: 3, fechaHora: 'Jueves 13/11 - 13:00 hs', disponible: true}
+];
